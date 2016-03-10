a203c5be4616d98c24b7d6526f9a8f9024c4059e
jmona789 <jmona789@gmail.com>
2016-03-10 16:45:55 -0500

    created very basic home page

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+//Node packages
 var express = require("express");
 var app = express();
 var request = require("request");
 var cheerio = require("cheerio");
 var expressHandlebars = require("express-handlebars");
+var bodyparser = require("body-parser");
+var PORT = process.env.PORT || 9001;
 
 //Handlebars set up
 app.engine("handlebars", expressHandlebars({
@@ -20,6 +23,10 @@ db.on("error", function(err) {
   console.log("Database Error:", err);
 });
 
-app.listen(3000, function() {
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.listen(PORT, function() {
   console.log("App running on port %s", PORT);
 });
